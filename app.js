b36a5bd1c319ac28cd252459df34a3a6958fab3d
0xpiyushpandeyceh <0xpiyushpandey@gmail.com>
2018-10-10 20:16:07 +0530

    App.js mongo commented

--- app.js
+++ app.js
@@ -29,9 +29,9 @@ mongoose.connect(config.database);
 let db = mongoose.connection;
 
 // Check connection
-db.once('open', function(){
-  console.log('Connected to MongoDB');
-});
+// db.once('open', function(){
+//   console.log('Connected to MongoDB');
+// });
 
 // Check for DB errors
 db.on('error', function(err){
