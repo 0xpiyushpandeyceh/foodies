c7069aed265615fa16223b02e707132a28927ffb
0xpiyushpandeyceh <0xpiyushpandey@gmail.com>
2018-10-10 20:13:07 +0530

    Hllo

---
+++ app.js
@@ -0,0 +1,110 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+const flash = require('connect-flash');
+var expressValidator = require('express-validator');
+
+var bodyParser = require('body-parser');
+var logger = require('morgan');
+// var mongo = require('mongodb');
+var mongoose = require('mongoose');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var batchesRouter = require('./routes/batches');
+const config = require('./config/database');
+
+// Route Files
+
+
+var app = express();
+
+var swig= require('swig');
+app.engine('html', swig.renderFile);
+
+//Database Connection
+
+mongoose.connect(config.database);
+let db = mongoose.connection;
+
+// Check connection
+db.once('open', function(){
+  console.log('Connected to MongoDB');
+});
+
+// Check for DB errors
+db.on('error', function(err){
+  console.log(err);
+});
+
+// Bring in Models
+let User = require('./models/user');
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'html');
+
+// Body Parser Middleware
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Express Messages Middleware
+// app.use(require('connect-flash')());
+// app.use(function (req, res, next) {
+//   res.locals.messages = require('express-messages')(req, res);
+//   next();
+// });
+
+// Express Validator Middleware
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/batches', batchesRouter);
+
+// BodyParser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
